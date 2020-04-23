2e006f6554bd43bfc5cdedb9dc1dd5ef11178b2e
Marcel Lipczynski <marcellipczynski@gmail.com>
2020-04-23 21:33:29 +0200

    Added macros and measuring time

--- eratostenes.cpp
+++ eratostenes.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <time.h>
+#include <omp.h>
+
+#define M 10
+#define N 1000000000
+
 using namespace std;
+
+
 
 void eratostenes(bool* tab, unsigned int n) {
 	for (int i = 2; i * i <= n; i++) {
@@ -14,54 +22,64 @@ void eratostenes(bool* tab, unsigned int n) {
 }
 
 int main() {
-	int m, n, sqrt_bound;
+	int sqrt_bound;
 	bool* primes;
 	bool* tab;
-	cout << "lower bound: ";
-	cin >> m;
-	cout << "upper bound: ";
-	cin >> n;
-	sqrt_bound = sqrt(n);
+	cout << "lower bound: " << M << "\n";
+	cout << "upper bound: " << N << "\n";
+	sqrt_bound = sqrt(N);
 	primes = new bool[sqrt_bound + 1];
 
-	if (m >= sqrt_bound) {
+	clock_t start, stop;
+	double omp_start = omp_get_wtime();
+
+	start = clock();
+
+	if (M >= sqrt_bound) {
 		for (int i = 2; i <= sqrt_bound; i++) {
 			primes[i] = 0;
 		}
 		eratostenes(primes, sqrt_bound);
 	}
 
-	tab = new bool[n + 1];
+	tab = new bool[N + 1];
 
-	if (m >= sqrt_bound) {
-		for (int i = 2; i <= n; i++) {
+	if (M >= sqrt_bound) {
+		for (int i = 2; i <= N; i++) {
 			tab[i] = 0;
 		}
 		for (int i = 0; i <= sqrt_bound; i++) {
 			if (primes[i] == 0) {
-				int start = m / i;
-				start = start * i == m ? start * i : start * i + i;
-				for (int j = start; j <= n; j += i) {
+				int start = M / i;
+				start = start * i == M ? start * i : start * i + i;
+				for (int j = start; j <= N; j += i) {
 					tab[j] = 1;
 				}
 			}
 		}
-		for (int i = m; i <= n; i++) {
+		for (int i = M; i <= N; i++) {
 			if (tab[i] == 0) {
 				cout << i << "\t";
 			}
 		}
 	}
 	else {
-		for (int i = 2; i <= n; i++) {
+		for (int i = 2; i <= N; i++) {
 			tab[i] = 0;
 		}
-		eratostenes(tab, n);
-		for (int i = m; i <= n; i++) {
+		eratostenes(tab, N);
+		/*for (int i = M; i <= N; i++) {
 			if (tab[i] == 0) {
 				cout << i << "\t";
 			}
-		}
+		}*/
 	}
+
+	stop = clock();
+	double omp_stop = omp_get_wtime();
+
+	printf("Czas pracy procesorow wynosi %f sekund\n", ((double)(stop - start) / 1000.0));
+	printf("Czas rzeczywisty wynosi %f sekund\n", ((double)(omp_stop - omp_start)));
+
 	return 0;
 }
