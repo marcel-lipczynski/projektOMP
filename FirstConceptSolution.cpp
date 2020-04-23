2ec4e177c113b9bdd0e62a77acc8c20711993a0a
joskar1999 <joskar1999@gmail.com>
2020-04-23 21:05:42 +0200

    my commit

--- FirstConceptSolution.cpp
+++ FirstConceptSolution.cpp
@@ -2,31 +2,31 @@
 #include <math.h>
 #include <vector>
 
-#define M 40
-#define N 49
+#define M 2
+#define N 2500000
 
 using namespace std;
 
-int main() {
+int main2() {
 
-	vector<int> primeNumbers{2};
-	if(M==2) printf("%d ", primeNumbers[0]);
-	
+	vector<int> primeNumbers{ 2 };
+	if (M == 2) printf("%d ", primeNumbers[0]);
+
 	for (int i = 3; i <= N; i++) {
 		bool isDivided = false;
-		
+
 		for (int j = 0; primeNumbers[j] <= sqrt(i) && j <= primeNumbers.size(); j++) {
 			if (i % primeNumbers[j] == 0) {
 				isDivided = true;
 				break;
 			}
-		
+
 		}
 		if (!isDivided) {
 			primeNumbers.push_back(i);
-			if(i >= M) printf("%d ", i);
+			if (i >= M) printf("%d ", i);
 		}
 	}
+	return 0;
 
-	
 }
